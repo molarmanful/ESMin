15d9691543fed105e04239b1cc4f5c6128a15537
Ben Pang <benjinpang@gmail.com>
2015-10-19 17:46:16 -0700

    rename lang, update interpreter

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# FOES-6
-Function-Only ES6. It is a language attempts to simplify ES6 significantly and improve its code-golfing abilities.
+# ESMin
+ESMin is a stack-based code-golfing language based on ES6.
 
 ## Other Notes
 Included is a parser, written in ES6. Language documentation will be added to the wiki.
--- interpreter.js
+++ interpreter.js
@@ -1,8 +1,8 @@
 var stack=[],//stack
 		
 		//I/O functions
-		_=prompt(),//input
-		$=i=>alert(i),//output
+		_=i=>stack.push(prompt`Input:`),//input
+		$=i=>alert(i!=[]._?i:stack),//output
 		
 		//stack functions
 		p=i=>stack.push(i),//push to stack
@@ -10,20 +10,18 @@ var stack=[],//stack
 		s=i=>stack.splice(i!=[]._?i:g()),//splice item from stack
 		c=i=>stack=[],//clear stack
 		
-		//operators
-		A=()=>g(-2)+g(),//add top 2 numbers in stack
-		S=()=>g(-2)-g(),//subtract top 2 numbers in stack
-		T=()=>g(-2)*g(),//multiply top 2 numbers in stack
-		D=()=>g(-2)/g(),//divide top 2 numbers in stack
-		M=()=>g(-2)%g(),//modulo/remainder top 2 numbers in stack
+		//extra operators
 		P=(x,i)=>Math.pow(i!=[]._?i:g(),x),//number^exponent
-		R=(x=0,y=0)=>0|Math.random()*(y-x+1)+x,//random
+		R=(x,y)=>0|Math.random()*(y-x+1)+x,//random
 		
 		//other functions and builtins
 		i=(c,f,e)=>eval(`if(${c}){${f}}else{${e}}`),//if/else conditional statements
 		l=(c,f)=>eval(`for(${c}){${f}}`),//loop
 		e=eval,
-		a=Array,
-		s=String,
-		r=RegExp,
-		m=Math;
+		m=Math,
+interpret=i=>{
+	stack=[];
+	i=document.getElementById('c').value;
+	i+=i.match(/\$/g)||';$()';
+	eval(i);
+}
