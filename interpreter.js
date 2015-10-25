b072f2c2e306b9efc3914c86fd3d94312bab3b87
Ben Pang <benjinpang@gmail.com>
2015-10-25 16:48:05 -0700

    completely redo aliasing

--- interpreter.js
+++ interpreter.js
@@ -1,53 +1,47 @@
-var interpret=i=>{
-	var stack=[],//stack
-		//I/O functions
-		_=i=>[i=prompt`Input:`,stack.push(i)][0],//input
-		$=i=>alert(i!=[]._?i:stack),//output
-			
-		//stack functions
-		p=i=>stack.push(i),//push to stack
-		g=i=>i!=[]._?stack[i<0?stack.length+i:i]:stack[stack.length-1],//get item from stack
-		s=i=>stack.splice(i!=[]._?i:stack.length-1),//splice item from stack
-		c=i=>stack=[],//clear stack
-			
-		//extra math stuff
-		P=(i='g()¢2',j)=>[i=i.split`¢`,j=Math.pow(i[0]!=[]._?eval(i[0]):g(),i[1]!=[]._?eval(i[1]):2),p(j)][1],//number^exponent
-		R=(i='0¢1',j,x,y)=>[i=i.split`¢`,j=0|Math.random()*(i[1]-i[0]+1)+i[0],p(j)][1],//random
-			
-		//other functions and builtins
-		i=i=>(i=i.split`/([^\\])¢`,eval(`if(${i[0]}){${i[1]}}else{${i[2]}}`)),//if/else conditional statements
-		l=i=>(i=i.split`/([^\\])¢`,eval(`for(${i[0]}){${i[1]}}`)),//loop
-		S=i=>[i=i!=[]._?document.getElementById('c').value[i]:document.getElementById('c').value,stack.push(i)][0],//get source code chars
-		e=eval,
-		M=Math,
-		r=(i,j)=>(p(j=[...i].reverse().join``),j),//reverse
-		L=i=>i.length,//get length
-		ś=(i='g()¢',j=i.split(/([^\\])¢/g),x)=>(p(x=j[0].split(j[1])),x),//split
-		j=(i='g()¢',k=i.split(/([^\\])¢/g),x)=>(p(x=k[0].join(k[1])),x),//join
-		m=(i='g()¢',j=i.split(/([^\\])¢/g))=>eval(j[0]).map(eval(j[1])),//map
-		ř=(i='g()¢',j=i.split(/([^\\])¢/g),x)=>(p(x=eval(j[0]).replace(eval(j[1]),eval(j[2]))),x),//replace
-		š=(i='g()¢',j=i.split(/([^\\])¢/g),x)=>(p(x=eval(j[0]).toString(eval(j[1]))),x),//toString
-		
-		//syntax from esmin to es6
-		i=document.getElementById('c').value
-			.replace(/([^\\])@/g,'$1()')
-			.replace(/~>/g,'=()=>')
-			.replace(/([pgs$])(-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?)/gm,'$1($2)')
-	
-			.replace(/([_$a-zA-Z]*)“(.*)”(.*)?/gm,'$1(`$2`)')
-			.replace(/([_$a-zA-Z]*)“(.*)$/gm,'$1(`$2`)')
-	
-			.replace(/([_$a-zA-Z]*)\((.*)\)(.*)?/gm,'$1($2)')
-	    .replace(/([_$a-zA-Z]*)\((.*)$/gm,'$1($2)')
-	
-			.replace(/([_$a-zA-Z]*)\[(.*?)\](.*)?/gm,'$1[$2]')
-	    .replace(/([_$a-zA-Z]*)\[(.*?)$/gm,'$1[$2]')
-	
-			.replace(/([_$a-zA-Z]*){(.*?)}(.*)?/gm,'$1{$2}')
-	    .replace(/([_$a-zA-Z]*){(.*?)$/gm,'$1{$2}')
-	
-			.replace(/([^\\])∞/g,'$1Infinity')
-		  .replace(/…/g,'...');
-	if(!i.match(/\$[^{]/g))i+=';$()';
-	eval(i);
+var interpret=c=>{
+	var Ξ=[],//stack
+			//I/O functions
+			î=i=>[i=prompt`Input:`,Ξ.push(i)][0],//input
+			ô=i=>alert(i!=[]._?i:Ξ),//output
+			ℹ=i=>[i=i!=[]._?c[i]:c,stack.push(i)][0],//source
+
+
+			//stack functions
+			ᵖ=(i=0)=>{Array.prototype.slice.call(arguments).map(x=>Ξ.push(x))},
+			ᵍ=i=>i!=[]._?Ξ[i<0?Ξ.length+i:i]:Ξ[Ξ.length-1],
+			ʳ=(i=Ξ.length-1)=>Ξ.splice(i),
+			ᶜ=i=>Ξ=[],
+
+			//super-basic aliasing
+			ī=Infinity,
+			ü=[]._,
+			М=Math,
+			Ϛ=String,
+			Ѧ=Array,
+			П=Number,
+			Σ=eval,
+
+			//syntax from esmin to es6
+			c=c
+				.replace(/⬮/g,'()')
+				.replace(/(.+)⤇/g,'($1)=>')
+				.replace(/⇏/g,'=()=>')
+				.replace(/([ᵖᵍʳᶜô])(-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?)/g,'$1($2)')
+				.replace(/([_$a-zA-Z]*)“([^”]*?)$/gm,'$1(`$2`)')
+				.replace(/([_$a-zA-Z]*)‘([^’]*?)$/gm,'$1(\\`$2\\`)')
+				.replace(/([_$a-zA-Z]*)\(([^)]*?)$/gm,'$1($2)')
+				.replace(/([_$a-zA-Z]*)\[([^\]]*?)$/gm,'$1[$2]')
+				.replace(/([_$a-zA-Z]*){([^}]*?)$/gm,'$1{$2}')
+				.replace(/…/g,'...')
+				.replace(/˖/g,'+=')
+				.replace(/˗/g,'-=')
+				.replace(/×/g,'*=')
+				.replace(/÷/g,'/=')
+				.replace(/¡/g,'!!')
+				.replace(/≔/g,'==')
+				.replace(/≠/g,'!=')
+				.replace(/⅋/g,'&&')
+				.replace(/ǁ/g,'||')
+	if(!c.match(/ô/g))c+=';ô()';
+	eval(c);
 }
