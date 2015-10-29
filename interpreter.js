90452474a07d3102e427fc898177ef5692ebef60
Ben Pang <benjinpang@gmail.com>
2015-10-28 20:28:08 -0700

    better eval, more arrow functions

--- interpreter.js
+++ interpreter.js
@@ -1,33 +1,33 @@
-var interpret=c=>{
-	var Ξ=[],//stack
-			//I/O functions
-			î=i=>[i=prompt`Input:`,Ξ.push(i),i][0],//input
-			ô=i=>alert(i!=[]._?i:JSON.stringify(Ξ)),//output
-			ℹ=i=>[i=i!=[]._?document.getElementById("c").value[i]:document.getElementById("c").value,Ξ.push(i)][0],//source
+
+var Ξ=[],//stack
+		//I/O functions
+		î=i=>[i=prompt`Input:`,Ξ.push(i),i][0],//input
+		ô=i=>alert(i!=[]._?i:JSON.stringify(Ξ)),//output
+		ℹ=i=>[i=i!=[]._?document.getElementById("c").value[i]:document.getElementById("c").value,Ξ.push(i)][0],//source
 
 
-			//stack functions
-			ᵖ=(i=0)=>{Array.prototype.slice.call(arguments).map(x=>Ξ.push(x))},
-			ᵍ=i=>i!=[]._?Ξ[i<0?Ξ.length+i:i]:Ξ[Ξ.length-1],
-			ʳ=(i=Ξ.length-1)=>Ξ.splice(i),
-			ᶜ=i=>Ξ=[],
-			
-			//extra functions
-			Ǝ=i=>eval(LZString.decompress(i)), //decode
-			ɘ=i=>LZString.decompress(i), //decodeval
+		//stack functions
+		ᵖ=(i=0)=>{Array.prototype.slice.call(arguments).map(x=>Ξ.push(x))},
+		ᵍ=i=>i!=[]._?Ξ[i<0?Ξ.length+i:i]:Ξ[Ξ.length-1],
+		ʳ=(i=Ξ.length-1)=>Ξ.splice(i),
+		ᶜ=i=>Ξ=[],
 
-			//super-basic aliasing
-			ī=Infinity,
-			ü=[]._,
-			М=Math,
-			Ϛ=String,
-			Ѧ=Array,
-			П=Number,
-			Ø=Object,
-			Ĵ=JSON,
-			Σ=eval,
-			ɼ=RegExp;
+		//extra functions
+		Ǝ=i=>interpret(LZString.decompress(i)),
+		ɘ=i=>LZString.decompress(i),
+		Σ=i=>interpret(i),
 
+		//super-basic aliasing
+		ī=Infinity,
+		ü=[]._,
+		М=Math,
+		Ϛ=String,
+		Ѧ=Array,
+		П=Number,
+		Ø=Object,
+		Ĵ=JSON,
+		ɼ=RegExp,
+interpret=c=>{
 	//syntax from esmin to es6
 	c=c
 		.replace(/î/g,'î()')
@@ -40,7 +40,8 @@ var interpret=c=>{
 			.replace(/⬮/g,'()')
 			.replace(/⇏/g,'(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 			.replace(/↛/g,'=(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
-			.replace(/↪/g,'=($,_,ã)=>')
+			.replace(/↪/g,'($,_,ã)=>')
+			.replace(/⤤/g,'=($,_,ã)=>')
 			.replace(/→/g,'=>')
 			.replace(/“/g,'(`')
 			.replace(/”/g,'`)')
