b0d0f05974825522d555808b601279efb3e7c8ce
Ben Pang <benjinpang@gmail.com>
2015-11-02 15:41:08 -0800

    moar arrows

--- interpreter.js
+++ interpreter.js
@@ -35,6 +35,8 @@ var Σ=(c,asdf=0)=>{
 		.replace(/↛/g,'=(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 		.replace(/↪/g,'($,_,ã)=>')
 		.replace(/⤤/g,'=($,_,ã)=>')
+		.replace(/⇝/g,'((a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
+		.replace(/⇀/g,'(($,_,ã)=>')
 		.replace(/→/g,'=>')
 		.replace(/“/g,'(`')
 		.replace(/”/g,'`)')
