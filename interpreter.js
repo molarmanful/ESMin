71db6fb7525bb307bb3e96ce0a86a32db3ec4807
Ben Pang <benjinpang@gmail.com>
2015-11-02 06:35:05 -0800

    do some period elimination

--- interpreter.js
+++ interpreter.js
@@ -94,6 +94,7 @@ var Σ=(c,asdf=0)=>{
 		.replace(/⅜/g,'(3/8)')
 		.replace(/⅝/g,'(5/8)')
 		.replace(/⅞/g,'(7/8)')
+		.replace(/([`)/\]])([\u00f8-\u01e5])/g,'$1["$2"]')
 	;
 	if(asdf==1e4&&!c.match(/ô/g))c+=';ô()';console.log(c);eval(c)
 }
