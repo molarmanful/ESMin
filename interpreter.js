427d31e93a6433ebfd31654d67982ee92674fbed
Ben Pang <benjinpang@gmail.com>
2015-11-02 17:26:55 -0800

    extend unicode values

--- interpreter.js
+++ interpreter.js
@@ -23,7 +23,8 @@ var Ξ=[],//stack
 		ש=window,
 		ɘ=i=>LZString.decompress(i);
 [Ϛ,Ѧ,П,Ø,ɼ,Ɗ].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v.prototype[String.fromCharCode(y+248)]=v.prototype[x]));
-[М,Ϛ,Ѧ,П,Ø,Ĵ,ɼ,Ɗ,ש].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v['ש'+String.fromCharCode(y+248)]=v[x]));
+[М,Ϛ,Ѧ,П,Ø,Ĵ,ɼ,Ɗ].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]=v[x]));
+[ש].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v[ש+String.fromCharCode(y+248)]=v[x]));
 var Σ=(c,asdf=0)=>{
 	//syntax from esmin to es6
 	c=c
@@ -97,7 +98,7 @@ var Σ=(c,asdf=0)=>{
 		.replace(/⅜/g,'(3/8)')
 		.replace(/⅝/g,'(5/8)')
 		.replace(/⅞/g,'(7/8)')
-		.replace(/([`)/\]])([\u00f8-\u01e5])/g,'$1["$2"]')
+		.replace(/([`)/\]])(ש?[\u00f8-\u10ffff])/g,'$1["$2"]')
 	;
 	if(asdf==1e4&&!c.match(/ô/g))c+=';ô()';console.log(c);eval(c)
 }
