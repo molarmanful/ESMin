130a341791c7c547fdef3ca034e3d15f5c8ca54f
Ben Pang <benjinpang@gmail.com>
2015-11-15 15:32:15 -0800

    bug fix

--- interpreter.js
+++ interpreter.js
@@ -55,7 +55,7 @@ var Σ=(c,asdf=0)=>{
 		.replace(/⇝/g,'((a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 		.replace(/⇀/g,'(($,_,ã)=>')
 		.replace(/§/g,'($1)')
-		.replace(/([ᵖᵍʳᶜôℹΣɘϚѦПØѨɼḌᴙξëß\u00f8-\u01ef])([A-Za-z$_ãïȉℇεȋɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\u1e00-\u1eff]|-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?)+/g,'$1($2)')
+		.replace(/([ᵖᵍʳᶜôℹΣɘϚѦПØѨɼḌᴙξëß\u00f8-\u01ef])(([A-Za-z$_ãïȉℇεȋɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\u1e00-\u1eff]|-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?)+)/g,'$1($2)')
 		.replace(/([ᵖᵍʳᶜôℹΣɘϚѦПØɼѨḌᴙξëß\u00f8-\u01ef])([^(\u00f8-\u01ef])/g,'$1($2')
 		.replace(/ï(\d+)/g,'ï[$1]')
 		.replace(/⇏/g,'(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
