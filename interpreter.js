37a52b920b8bd77265d389f732ebf5eac1770767
Ben Pang <benjinpang@gmail.com>
2015-11-21 15:31:49 -0800

    add numbers.js and helper functions

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,9 @@
+math.import(numbers,{wrap:true,silent:true});
 [String,Array,Number,Object,RegExp,Date,s,_,XRegExp,Function].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v.prototype[String.fromCharCode(y+248)]=v.prototype[x]));
 [math,String,Array,Number,Object,JSON,RegExp,Date,_,s,window,Function].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]=v[x]));
+//get function alias
+var alias=(v,w)=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]==v[w]?String.fromCharCode(y+248):0).join``.replace(/0/g,'');
+
 var Ξ=[],//stack
 		//I/O functions
 		ô=i=>o.value+=i!=[]._?i:Ξ.join`\n`,//output
