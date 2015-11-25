1e461abf4fc040e2f4402a9cb68d0ea4d68a372e
Ben Pang <benjinpang@gmail.com>
2015-11-24 20:39:06 -0800

    bug fixes

--- interpreter.js
+++ interpreter.js
@@ -52,8 +52,8 @@ shoco.d=i=>shoco.decompress(new Uint8Array((i.constructor==Array?i[0]:i).split``
 var compress=i=>LZString.compress(shoco.c(i));
 
 var Σ=c=>{
-	var r='ᵖᵍʳôℹΣɘϚѦПØѨɼⱤƒḌⱮᴙᶊëßᶏ\u00f8-\u0236',
-			n='A-Za-z$_ãïîíìịʉℇεᴉɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\u1e00-\u1eff',
+	var r='ᵖᵍʳôℹΣɘϚѦПØѨɼⱤƒḌⱮᴙᶊëßᶏ\\u00f8-\\u0236',
+			n='A-Za-z$_ãïîíìịʉℇεᴉɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\\u1e00-\\u1eff',
 			d='-?\\d+(?:\\.\\d*)?(?:e[+\\-]?\\d+)?';
 	//syntax from esmin to es6
 	c=c
