d1fcb70e429da21f3a9c2f00e6b65effe5e41bb8
Ben Pang <benjinpang@gmail.com>
2015-12-02 19:44:33 -0800

    add array alias

--- interpreter.js
+++ interpreter.js
@@ -78,6 +78,7 @@ var Σ=c=>{
 		.replace(/⨝/g,'ø⬯')
 		.replace(/ⓑ/g,'ß2')
 		.replace(/ⓜ/g,'ć⇀')
+		.replace(/Ⓐ(.+)/g,'⟦Ѧ($1)]')
 		.replace(/⬮/g,'()')
 		.replace(/⬯/g,'(``)')
 		.replace(/⬭/g,'(` `)')
