01698a120d2854f5e38447de46313665d71152d2
Ben Pang <benjinpang@gmail.com>
2015-12-09 17:53:39 -0800

    make more doc-friendly

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,9 @@
+//Welcome to the annotated source code for 𝔼𝕊𝕄𝕚𝕟, a wonderful JavaScript golfing language!
+//This will serve as documentation for those who want to learn the language.
+
 //this is where the numbers.js library ends up
 math.import(numbers,{wrap:true,silent:true});
+
 //prototype-based function aliasing
 [String,Array,Number,Object,RegExp,Date,s,_,XRegExp,Function].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v.prototype[String.fromCharCode(y+248)]=v.prototype[x]));
 //object-based function aliasing
@@ -8,20 +12,28 @@ math.import(numbers,{wrap:true,silent:true});
 var alias=(v,w)=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]==v[w]?String.fromCharCode(y+248):0).join``.replace(/0/g,'');
 
 var Ξ=[],//stack representation
-		//I/O functions
+		
+		//I/O FUNCTIONS
 		//not shown: ï = input, î = ï[0], í = ï[1], ì = ï[2]
-		ô=i=>o.value+=i!=[]._?i:Ξ.join`\n`,//output
-		ℹ=i=>[i=i!=[]._?c.value[i]:c.value,Ξ.push(i)][0],//source
+		//output
+		ô=i=>o.value+=i!=[]._?i:Ξ.join`\n`,
+		//source
+		ℹ=i=>[i=i!=[]._?c.value[i]:c.value,Ξ.push(i)][0],
 
-		//stack functions
-		ᵖ=(i=0,...r)=>{Ξ.push(i,...r)},//push
-		ᵍ=i=>i!=[]._?Ξ[i<0?Ξ.length+i:i]:Ξ[Ξ.length-1],//get
-		ʳ=(i=Ξ.length-1)=>Ξ.splice(i,1),//remove
-		ᶜ=i=>Ξ=[],//clear
+		//STACK FUNCTIONS
+		//push
+		ᵖ=(i=0,...r)=>{Ξ.push(i,...r)},
+		//get
+		ᵍ=i=>i!=[]._?Ξ[i<0?Ξ.length+i:i]:Ξ[Ξ.length-1],
+		//remove
+		ʳ=(i=Ξ.length-1)=>Ξ.splice(i,1),
+		//clear
+		ᶜ=i=>Ξ=[],
 
-		//more aliases!
+		//MORE ALIASES
 		ị=Infinity,
-		ʉ=[]._,//undefined
+		//undefined
+		ʉ=[]._,
 		ℇ=math.e,
 		ε=Number.EPSILON,
 		ᴉ=math.sqrt(-1),
@@ -30,9 +42,11 @@ var Ξ=[],//stack representation
 		τ=math.pi*2,
 		М=math,
 		Ϛ=String,
-		ᶊ=s,//underscore.string
+		//underscore.string
+		ᶊ=s,
 		Ѧ=Array,
-		Ѩ=_,//lodash
+		//lodash
+		Ѩ=_,
 		П=Number,
 		Ø=Object,
 		ʝ=JSON,
@@ -44,15 +58,18 @@ var Ξ=[],//stack representation
 		ß='toString',
 		Ꝉ='length',
 		ᶏ='apply',
-		Ɱ=(i,f,s='',j='')=>typeof i=='object'?i.map(f):typeof i=='string'?i.split(s).map(f).join(j):eval((''+i).split(s).map(f).join(j)),//map
-		ᴙ=i=>[...i].reverse().join``;//reverse
+		//general-purpose map
+		Ɱ=(i,f,s='',j='')=>typeof i=='object'?i.map(f):typeof i=='string'?i.split(s).map(f).join(j):eval((''+i).split(s).map(f).join(j)),
+		//string/number reverse
+		ᴙ=i=>[...''+i].reverse().join``;
 String.prototype.ċ=function(x,y=''){return this.replace(x,y)};//improved replace function
 String.prototype.ė=function(r,o='',t){t=this.replace(r,o);return t!=this?t.ė(r,o):t};//recursive replace
 var Մ=(i,r,o='',t)=>(t=i.replace(r,o),t!=i?Մ(t,r,o):t),//recursive replace (prefix-style)
 		ë=eval,
 		Յ='0b',
 		ᛪ='0x',
-		nchars=`ḀḁḂḃḄḅḆḇḈḉḊḋḌḍḎḏḐḑḒḓḔḕḖḗḘḙḚḛḜḝḞḟḠḡḢḣḤḥḦḧḨḩḪḫḬḭḮḯḰḱḲḳḴḵḶḷḸḹḺḻḼḽḾḿṀṁṂṃṄṅṆṇṈṉṊṋṌṍṎṏṐṑṒṓṔṕṖṗṘṙṚṛṜṝṞṟṠṡṢṣṤṥṦṧṨṩṪṫṬṭṮṯṰṱṲṳṴṵṶṷṸṹṺṻṼṽṾṿẀẁẂẃẄẅẆẇẈẉẊẋẌẍẎẏẐẑẒẓẔẕẖẗẘẙaʾẛẜẝẞẟẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹỺỻỼỽỾỿ`//number aliases for 0-256; get number alias using nchars[5]
+		//number aliases for 0-256; get number alias using nchars[5]
+		nchars=`ḀḁḂḃḄḅḆḇḈḉḊḋḌḍḎḏḐḑḒḓḔḕḖḗḘḙḚḛḜḝḞḟḠḡḢḣḤḥḦḧḨḩḪḫḬḭḮḯḰḱḲḳḴḵḶḷḸḹḺḻḼḽḾḿṀṁṂṃṄṅṆṇṈṉṊṋṌṍṎṏṐṑṒṓṔṕṖṗṘṙṚṛṜṝṞṟṠṡṢṣṤṥṦṧṨṩṪṫṬṭṮṯṰṱṲṳṴṵṶṷṸṹṺṻṼṽṾṿẀẁẂẃẄẅẆẇẈẉẊẋẌẍẎẏẐẑẒẓẔẕẖẗẘẙaʾẛẜẝẞẟẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹỺỻỼỽỾỿ`
 		;
 //fix parentheses functions - borrowed from Japt (Thanks!)
 var subparen=e=>{for(var r=0,n=0,t=0;t<e.length;t++)"("==e[t]&&r++,")"==e[t]&&(r--,n=Math.min(n,r));return 0>n&&(e="(".repeat(-n)+e,r-=n),r>0&&(e+=")".repeat(r)),e},
@@ -63,16 +80,25 @@ shoco.c=i=>Array.prototype.map.call(shoco.compress(i),x=>String.fromCharCode(x))
 shoco.d=i=>shoco.decompress(new Uint8Array((i.constructor==Array?i[0]:i).split``.map(x=>x.charCodeAt(0))));
 var compress=i=>LZString.compress(shoco.c(i));
 
-//time to interpret!
+//TIME TO INTERPRET!
 var Σ=c=>{
-	var r='ᵖᵍʳôℹΣɘϚѦПØѨɼⱤƒᶁⱮᴙՄᶊëßᶏ\\u00f8-\\u0236',//functions
-			n='A-Za-z$_ãïîíìịʉℇεᴉɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\\u1e00-\\u1eff',//these don't need surrounding parens
-			d='-?\\d+(?:\\.\\d*)?(?:e[+\\-]?\\d+)?';//real numbers regex
-	//transpile syntax from esmin to es6
-	c=fixParens(c //fix parentheses after 1st-stage transpilation
-		//compression blocks
+	var 
+	    //functions
+	    r='ᵖᵍʳôℹΣɘϚѦПØѨɼⱤƒᶁⱮᴙՄᶊëßᶏ\\u00f8-\\u0236',
+			//these don't need surrounding parens
+			n='A-Za-z$_ãïîíìịʉℇεᴉɸπτ²³ⁿ√∛¼½¾⅐⅑⅒⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞ᶀᶍ\\u1e00-\\u1eff',
+			//real numbers regex
+			d='-?\\d+(?:\\.\\d*)?(?:e[+\\-]?\\d+)?';
+	
+	//TRANSPILE 𝔼𝕊𝕄𝕚𝕟 => ES6
+	//fix parentheses after 1st-stage transpilation
+	c=fixParens(c
+		//COMPRESSION
+		//mixed compression
 		.replace(/ɘ(.+)#/g,(x,y)=>shoco.d(LZString.decompress(y)))
+		//shoco compression
 		.replace(/э([^]+)#/gm,(x,y)=>shoco.d(y))
+		//LZString compression
 		.replace(/Э(.+)#/g,(x,y)=>LZString.decompress(y))
 		//zero series aliases
 		.replace(/ℍ/g,'00')
@@ -84,12 +110,18 @@ var Σ=c=>{
 		.replace(/𝔼/g,'000000000000000000')
 		.replace(/ℤ/g,'000000000000000000000')
 		.replace(/𝕐/g,'000000000000000000000000')
-		.replace(/⊕/g,'⬯+')//(``)+ => good for string coercion
-		.replace(/⨝/g,'ø⬯')//.join()
-		.replace(/ⓑ/g,'ß2')//.toString(2)
-		.replace(/ⓜ/g,'ć⇀')//.map(($,_,ã)=>
-		.replace(/ⓢ/g,'Č⬯')//.split(``)
-		.replace(eval(`/Ⓐ(([${n}]|${d})+)/g`),'⟦Ѧ($1)]')//[...Array(n)]
+		//(``)+ => good for string coercion
+		.replace(/⊕/g,'⬯+')
+		//.join()
+		.replace(/⨝/g,'ø⬯')
+		//.toString(2)
+		.replace(/ⓑ/g,'ß2')
+		//.map(($,_,ã)=>
+		.replace(/ⓜ/g,'ć⇀')
+		//.split(``)
+		.replace(/ⓢ/g,'Č⬯')
+		//[...Array(n)]
+		.replace(eval(`/Ⓐ(([${n}]|${d})+)/g`),'⟦Ѧ($1)]')
 		//function argument aliases
 		.replace(/⬮/g,'()')
 		.replace(/⬯/g,'(``)')
@@ -100,35 +132,37 @@ var Σ=c=>{
 		.replace(/␉/g,'\t')
 		.replace(/␠/g,'\s')
 		.replace(/⍘(.)/g,'(`$1`)')//one-char string; can be used as function argument
-		//parenthetical arrow functions
+		//PARENTHETICAL ARROW FUNCTIONS
 		.replace(/⇝/g,'((a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 		.replace(/⇀/g,'(($,_,ã)=>')
 		.replace(/ª/g,'ᶏש,')//.apply(window,
-		//auto-adding parentheses
+		//AUTO-ADD PARENTHESES
 		.replace(eval(`/([${r}])(([${n}]|${d})+)/g`),'$1($2)')
 		.replace(eval(`/([${r}])([^(\u00f8-\u0236ßꝈᶏ¤])/g`),'$1($2')
-		.replace(/¤/g,'')//prevents object from turning into a function
-		//auto-adding brackets
+		//prevents object from turning into a function
+		.replace(/¤/g,'')
+		//AUTO-ADD BRACKETS
 		.replace(eval(`/⎖(([${n}]|\\d)+)/g`),'[$1]')
 		.replace(/([ïîíì])(\d+)/g,'$1[$2]')
-		//more arrow functions
+		//MORE ARROW FUNCTIONS
 		.replace(/⇏/g,'(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 		.replace(/↛/g,'=(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
 		.replace(/↪/g,'($,_,ã)=>')
 		.replace(/⤤/g,'=($,_,ã)=>')
 		.replace(/→/g,'=>')
-		//parentheses aliases
+		//PARENTHESES
 		.replace(/”/g,'`)')
 		.replace(/‘/g,'\\`')
 		.replace(/’/g,'\\`)')
 		.replace(/⸩/g,'))')
 		.replace(/⎞/g,'/)')
-		.replace(/⦃/g,'${')//template string
-		//spread operator
+		//template string start
+		.replace(/⦃/g,'${')
+		//SPREAD OPERATORS
 		.replace(/…/g,'...')
 		.replace(eval(`/⟬([${n}]+)/g`),'[...$1]')
 		.replace(/⟦/g,'[...')
-		//math assignment operators
+		//MATH ASSIGNMENT OPERATORS
 		.replace(/˖/g,'+=')
 		.replace(/⧺/g,'++')
 		.replace(/˗/g,'-=')
@@ -136,7 +170,7 @@ var Σ=c=>{
 		.replace(/×/g,'*=')
 		.replace(/÷/g,'/=')
 		.replace(/٪/g,'%=')
-		//logic/coercion
+		//LOGIC
 		.replace(/‼/g,'!!')
 		.replace(/≔/g,'==')
 		.replace(/≠/g,'!=')
@@ -144,19 +178,19 @@ var Σ=c=>{
 		.replace(/≥/g,'>=')
 		.replace(/⅋/g,'&&')
 		.replace(/⋎/g,'||')
-		//some more keywords
+		//more keywords
 		.replace(/ⱳ/g,'with(')
 		.replace(/ᶉ/g,'return ')
 		.replace(/ₙ/g,'new ')
 		.replace(/ᕠ/g,'this')
-		//exponents
+		//EXPONENTS/ROOTS
 		.replace(eval(`/(⦅.+|(([${n}]|${d})+))²/g`),'Math.pow($1,2)')
 		.replace(eval(`/(⦅.+|(([${n}]|${d})+))³/g`),'Math.pow($1,3)')
 		.replace(eval(`/(⦅.+|(([${n}]|${d})+))ⁿ(.+⦆|(([${n}]|${d})+))/g`),'Math.pow($1,$4)')
 		.replace(eval(`/√(.+⦆|(([${n}]|${d})+))/g`),'Math.sqrt($1)')
 		.replace(eval(`/∛(.+⦆|(([${n}]|${d})+))/g`),'Math.cbrt($1)')
 		.replace(/[⦅⦆]/g,'')
-		//fractions
+		//FRACTIONS
 		.replace(/½/g,'.5')
 		.replace(/¼/g,'.25')
 		.replace(/¾/g,'.75')
@@ -175,14 +209,15 @@ var Σ=c=>{
 		.replace(/⅜/g,'(3/8)')
 		.replace(/⅝/g,'(5/8)')
 		.replace(/⅞/g,'(7/8)')
-		//
+		//BINARY/HEX PREFIXES
 		.replace(/ᶀ/g,'0b')
 		.replace(/ᶍ/g,'0x')
-		.replace(/Ⅹ/g,'10')//alias for 10; can be used with zeroes series
-		//detecting object/prototype functions
+		//alias for 10; can be used with zeroes series
+		.replace(/Ⅹ/g,'10')
+		//AUTO-FORMAT FUNCTION NAMES
 		.replace(/([^.])([\u00f8-\u0236])/g,'$1["$2"]')
 		.replace(/([ßꝈᶏ])/g,'[$1]')
-		//regex aliases
+		//REGEX
 		.replace(/⑴/g,'$1')
 		.replace(/⑵/g,'$2')
 		.replace(/⑶/g,'$3')
@@ -194,23 +229,23 @@ var Σ=c=>{
 		.replace(/ᶌ/g,'aeiou')
 		.replace(/⌿/g,'/g')
 		.replace(/⍀/g,'/ig')
-		//more keywords
+		//some more keywords
 		.replace(/⊨/g,'true')
 		.replace(/⊭/g,'false')
 		.replace(/␀/g,'null')
-		//bitshifts
+		//BITSHIFTS
 		.replace(/«/g,'<<')
 		.replace(/»/g,'>>')
 		.replace(/⫸/g,'>>>')
 		//bitwise rounding
 		.replace(/˜/g,'~~')
 	)
-		//for loop (@ = end parentheses)
+		//FOR LOOP (@ = end parentheses)
 		.replace(/↺/g,'for(')
 		.replace(/([^\\])@/g,'$1)');
 	
-	//outputting
+	//OUTPUT
 	if(!c.match(/ô/g)&&c.match(/ᵖ/g)){c+=';ô()',console.log(c),eval(c);return}//implicit (stack) output
 	else if(Ξ.length<1&&!c.match(/ô/g)){console.log(c),o.value=eval(c);return}//implicit output
-	else console.log(c),eval(c); //explicit output
+	else console.log(c),eval(c);//explicit output
 }
