0d1c1b6baeb6bb206bf26c35edf802b66a6c4038
Ben Pang <benjinpang@gmail.com>
2015-12-12 16:32:05 -0800

    formatting

--- interpreter.js
+++ interpreter.js
@@ -149,9 +149,9 @@ var Σ=c=>{
 		.replace(/⍘(.)/g,'(`$1`)')
 		//PARENTHETICAL ARROW FUNCTIONS
 		.replace(/⇝/g,'((a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)=>')
+    .replace(/⇀/g,'(($,_,ã)=>')
 		//ARGUMENT PARSER: turns `⁽abc 5` into `(a,b,c,5)`.
 		.replace(/⎝/g,`=⁽`)
-		.replace(/⇀/g,'(($,_,ã)=>')
 		//PARENTHETICAL ARGUMENT PARSER
 		.replace(/⎛/g,'(⁽')
     .replace(eval(`/⁽(([${n}]|${d}| )+)/g`),(x,y)=>`(${y.split(/\s|/g)})`)
