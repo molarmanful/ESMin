35cdb2f57dd27167c8bec0c9a8ad3c762d8f1bfe
Ben Pang <benjinpang@gmail.com>
2016-03-24 20:53:25 -0700

    oops

--- interpreter3.js
+++ interpreter3.js
@@ -42,7 +42,7 @@ Math.bn=(x,y=x.length)=>Combinatorics.baseN(x,y).toArray();
 //prototype-based function aliasing
 [String,Array,Number,Object,RegExp,Date,s,_,XRegExp,Function,Pen,numeral,jQuery].map(v=>Object.getOwnPropertyNames(v.prototype).map((x,y)=>v.prototype[String.fromCharCode(y+248)]=v.prototype[x]));
 //object-based function aliasing
-[math,String,Array,Number,Object,JSON,RegExp,XRegExp,Date,_,s,window,Function,numeral,jQuery].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]=v[x]));
+[math,Math,String,Array,Number,Object,JSON,RegExp,XRegExp,Date,_,s,window,Function,numeral,jQuery].map(v=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]=v[x]));
 //function alias helper: use as `alias(Array,METHOD_NAME_STRING)`
 var alias=(v,w)=>Object.getOwnPropertyNames(v).map((x,y)=>v[String.fromCharCode(y+248)]==v[w]?String.fromCharCode(y+248):0).join``.replace(/0/g,'');
 //char palette helper
